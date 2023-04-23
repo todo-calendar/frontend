db0c65699cef8faf3fe55982edf837d1bc1693a5
natsu727 <himajin0717@gmail.com>
2023-04-23 10:31:01 +0900

    variable changes

--- popup.js
+++ popup.js
@@ -1,16 +1,16 @@
 const sendFormButton = document.getElementById("sendForm");
-let Data = { task: "0", limmit: "0", priority: "0" };
+let Data = { title: "0", endTime: "0", priority: "0" };
 sendFormButton.addEventListener("click", (e) => {
   let task = document.getElementById("task");
   let limmit = document.getElementById("limmit");
   let priority = document.getElementById("priority");
-  Data.task = task.value;
-  Data.limmit = limmit.value;
+  Data.title = task.value;
+  Data.endTime = limmit.value;
   Data.priority = priority.value;
   var schedule_Data = new XMLHttpRequest();
   schedule_Data.open(
     "POST",
-    "https://script.google.com/macros/s/AKfycbzEtX1qV4p_lbY2hvtZ2ZJefTdjes5cy2XEuGcVMG5VBAshjVgmcGTau552SNYdtWGCmA/exec"
+    "https://script.google.com/macros/s/AKfycby2zy7JOel7iZeo3-8b2IkiNZlpzQxj5sT98ea6zUSP4mNcOeYCm1hcxMzWSuuxc5jtVA/exec"
   );
   schedule_Data.setRequestHeader("content-type", "application/json");
   schedule_Data.send(JSON.stringify(Data));
